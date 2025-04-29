77e8fdaa716b1d67770311b19773a576c73c6dd7
brlopez192 <brlopez192@gmail.com>
2025-04-29 19:42:10 -0400

    Exercise 2.5

--- src/books/models.py
+++ src/books/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.shortcuts import reverse
 
 # Create your models here.
 
@@ -17,14 +18,18 @@ book_type_choices = (('hardcover', 'Hardcover'),
 
 class Book(models.Model):
     name = models.CharField(max_length=120)
+    author_name = models.CharField(max_length=120)
+    price = models.FloatField(help_text='in US dollars $')
     genre = models.CharField(max_length=120,
                              choices=genre_choices,
                              default='classic')
     book_type = models.CharField(max_length=120, 
                                  choices=book_type_choices, 
                                  default='hardcover')
-    price = models.FloatField(help_text='in US dollars $')
-    author_name = models.CharField(max_length=120)
+    pic = models.ImageField(upload_to='books', default='no_picture.jpg')
 
     def __str__(self):
         return str(self.name)
+ 
+    def get_absolute_url(self):
+        return reverse('books:detail', kwargs={'pk': self.pk})
--- src/books/tests.py
+++ src/books/tests.py
@@ -32,3 +32,9 @@ class BookModelTest(TestCase):
 
         # Compare the vale to the result
         self.assertEqual(max_length, 120)
+
+    def test_get_absolute_url(self):
+        book = Book.objects.get(id=1)
+        #get_absolute_url() should take you to the detail page of book #1
+        #and load the URL /books/list/1
+        self.assertEqual(book.get_absolute_url(), '/books/list/1')
